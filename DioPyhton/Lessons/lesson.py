9256b5e3675312b8aa8b879310feaa870caee906
Willian Florencio Calixto <142696121+WillianFlorencioCalixtom2W1@users.noreply.github.com>
2023-09-30 20:42:33 -0300

    Add new file

---
+++ DioPyhton/Lessons/lesson.py
@@ -0,0 +1,19 @@
+print(1+5)
+print(1.5 + 1)
+print(True)
+print(False)
+print('Willian')
+
+'''tipos 
+int
+boll  
+True
+False
+string/str'''
+idade = 18
+ano = 2002
+
+print(type(idade))
+print(type(ano))
+
+
